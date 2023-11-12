387a12cde263ba22e59433a30f385f715a8d17ea
monii <40794673+moniik@users.noreply.github.com>
2023-11-12 19:59:17 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,33 @@
 #!/usr/bin/env sh
 
-pwd
-ls -la 
-ADDON_NAME=$(ls $INPUT_APP_PATH)
-# ADDON_FULL_PATH=$(pwd)/$INPUT_APP_PATH
+# mount point: -v "/home/runner/work/<repo_name>/<repo_name>":"/github/workspace" 
+# if INPUT_APP_PATH = hoge
+# current dir /github/workspace in docker
+# ADDON_NAME=$(ls $INPUT_APP_PATH)
+# -> ls hoge
+# hoge.tar.gz
+# --> ADDON_NAME=hoge.tar.gz
+
+# ADDON_FULL_PATH="$INPUT_APP_PATH/$ADDON_NAME"
+# -> ADDON_FULL_PATH=hoge/hoge.tgz
+
+# Expected condition is to exists the following path in host if INPUT_APP_PATH=hoge.
+# /home/runner/work/<repo_name>/<repo_name>/hoge/hoge.tgz
+
+# current dir in host 
+# /home/runner/work/<repo_name>/<repo_name>
+# slim package ../<repo_name> 
+# output: /home/runner/work/<repo_name>/<repo_name>/<repo_name>.tar.gz
+# INPUT_APP_PATH have to be <repo_name> for ls command.
+# finally these condition is required.
+# INPUT_APP_PATH=<repo_name>
+# file in host: /home/runner/work/<repo_name>/<repo_name>/<repo_name>/<repo_name>.tgz
+# this is really complex.
+
+# just pass the tar.gz fie path which provided slim command
+# INPUT_APP_PATH=<repo_name>.tar.gz
+# /home/runner/work/<repo_name>/<repo_name>/<repo_name>.tar.gz
+# -> /github/workspace/<repo_name>.tar.gz
 ADDON_FULL_PATH=./$INPUT_APP_PATH
 
 echo "$INPUT_USERNAME" "$INPUT_PASSWORD" "$ADDON_FULL_PATH" "$INPUT_INCLUDED_TAGS" "$INPUT_EXCLUDED_TAGS" "$INPUT_LOG_LEVEL"
